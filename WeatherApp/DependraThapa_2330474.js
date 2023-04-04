471100a13c2f87752f349e6901c4ec6ec3b3444b
dependra thapa <np03cs4a220246@heraldcollege.edu.np>
2023-04-04 21:26:03 +0545

    updated code

--- WeatherApp/DependraThapa_2330474.js
+++ WeatherApp/DependraThapa_2330474.js
@@ -1,17 +1,19 @@
+//An object with the API key and base URL
 const api = {
   key: "254211217ada191d7ede899eb8b4ec41",
   base: "https://api.openweathermap.org/data/2.5/"
 };
-
+// Get the search box element and listen for keypress events
 const searchbox = document.getElementById('search-box');
 searchbox.addEventListener('keypress', setQuery);
 
+// When the user types the Enter key in the search box, get the weather data for the entered city
 function setQuery(evt) {
   if (evt.keyCode == 13) {
     getResults(searchbox.value);
   }
 }
-
+// Fetch weather data for the specified city from the API and display it on the page
 async function getResults (query) {
   const response = await  fetch(`${api.base}weather?q=${query}&units=metric&APPID=${api.key}`);
   const data = await response.json();
@@ -20,6 +22,7 @@ async function getResults (query) {
 }
 getResults("sevenoaks");
 
+// Update the HTML page with the weather data for the specified city
  function displayResults (weather) {
   let city = document.getElementById('city');
   city.innerText = `${weather.name}, ${weather.sys.country}`;
@@ -28,8 +31,6 @@ getResults("sevenoaks");
   let date = document.getElementById('date');
   date.innerText = dateBuilder(now);
 
-  // let time = document.getElementById('time');
-  // time.innerText =  timeBuilder(now);
 
   let temp = document.getElementById('temp');
   temp.innerHTML = `${weather.main.temp}<span>°c</span>`;
@@ -43,7 +44,7 @@ getResults("sevenoaks");
   let pressure = document.getElementById('pressure');
   pressure.innerText = `Pressure: ${weather.main.pressure} Pa`;
 }
-
+// Format the date as a string in a user-friendly format
 function dateBuilder (d) {
   let months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
   let days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
@@ -55,7 +56,7 @@ function dateBuilder (d) {
 
   return `${day} ${date} ${month} ${year}`;
 }
-
+// Format the time as a string in a user-friendly format
 function timeBuilder (d) {
   let hours = d.getHours();
   let minutes = d.getMinutes();
